60bed4b651e85c10f9032c47c67d47ca811a971e
GXL Ventures <geoffrey.single@gmail.com>
2025-07-11 11:44:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function initParticles() {
                         particles: {
                             number: { value: 150, density: { enable: true, value_area: 800 } },
                             color: { value: '#007bff' },
-                            //color: { value: ['#007bff', '#55565B', '#ff0000'] }
                             shape: { type: 'circle' },
                             opacity: { value: 1.0, random: false }, // Fully opaque
                             size: { value: 8, random: true },
